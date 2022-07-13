1a4caf5bf58335ff85682547b71f41eb61074466
Doomer77 <newzibkoff1984@gmail.com>
2022-07-13 19:23:40 +0300

    addeded tag p component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button } from "../components";
+import { Button, Ptag } from "../components";
 import { Htag } from "../components/HTag/HTag";
 
 export default function Home(): JSX.Element {
@@ -8,6 +8,9 @@ export default function Home(): JSX.Element {
       <Htag tag="h1">Text</Htag>
       <Button appearance="primary">Узнать подробнее</Button>
       <Button appearance="ghost">Узнать подробнее</Button>
+      <Ptag>Средний</Ptag>
+      <Ptag size="l">Большой</Ptag>
+      <Ptag size="s">Малый</Ptag>
     </>
   );
 }
